2f6f116d99856e5d8bc7c0bd6c8824e110405888
Arian Bahadoran <arianbig@gmail.com>
2020-05-03 20:42:03 +0300

    missle launched

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ from math import radians as Rad
 setAutoUpdate(False)
 
 # create Screen
-Pushed = 1
+Pushed = 2
 screenSize(980, 638)
 # add background image
 
@@ -54,68 +54,110 @@ moveSprite(meteor, meteorX, meteorY, True)
 rotateSprite(meteor, meteorangle)
 
 # make rocket
+R = -100
+rocketrX = 480
+rocketrY = 319
+rocketangle = (math.atan2(319 - rocketrY, 480 - rocketrX)) * 180 / math.pi + 90
 rocket = makeSprite("rocket1.gif")
 addSpriteImage(rocket, "rocket2.gif")
 addSpriteImage(rocket, "rocket3.gif")
 addSpriteImage(rocket, "rocket4.gif")
 addSpriteImage(rocket, "rocket5.gif")
 addSpriteImage(rocket, "rocket6.gif")
-showSprite(rocket)
-moveSprite(rocket, 480, 319,True)
-x = 90  # angle of ship
-y = 90  # angle of rocket
-z = 90
+moveSprite(rocket, 480, 319, True)
+x = 0  # angle of ship
+y = 0  # angle of rocket
 running = True
 launch = False
-rocketX = 0
-rocketY = 0
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.KEYUP and event.key == pygame.K_SPACE:
             launch = True
+        # RXLmode=rocketrX
+        # RYLmode=rocketrY
+        # RXAmode=rocketangle
 
     if keyPressed("esc"):
         running = False
     elif keyPressed("left"):  # rotation left
         rotateSprite(Ship, x)
-        rotateSprite(rocket, y)
-        y -= 1
+        # rotateSprite(rocket, -y)
+        y += 1
         x -= 1
-        z += 1
+        rocketrX = rocketrX + (R * math.sin(math.radians(y)))
+        print(rocketrX)
+        rocketrY = rocketrY - (R * (1 - math.cos(math.radians(y)))) + R
+        print(rocketrY)
+        rocketangle = (math.atan2(rocketrY - 319, rocketrX - 480)) * 180 / math.pi + y + 180
+        print(rocketangle)
+        rocketrX = 480
+        rocketrY = 319
+        # pause(15)
+
     elif keyPressed("right"):  # rotation right
         rotateSprite(Ship, x)
-        rotateSprite(rocket, y)
-        y += 1
+        # rotateSprite(rocket, -y)
+        y -= 1
         x += 1
-        z -= 1
+        rocketrX = rocketrX + (R * math.sin(math.radians(y)))
+        print(rocketrX)
+        rocketrY = rocketrY - (R * (1 - math.cos(math.radians(y)))) + R
+        print(rocketrY)
+        rocketangle = (math.atan2(rocketrY - 319, rocketrX - 480)) * 180 / math.pi + y + 180
+        print(rocketangle)
+        rocketrX = 480
+        rocketrY = 319
+        # pause(15)
+    if launch == True:
+        changeSpriteImage(rocket, 1)
+        pause(6)
+        changeSpriteImage(rocket, 2)
+        pause(6)
+        changeSpriteImage(rocket, 3)
+        pause(6)
+        changeSpriteImage(rocket, 4)
+        pause(6)
+        changeSpriteImage(rocket, 5)
+        pause(6)
     if (410 < meteorX < 542) and (257 < meteorY < 381):
         print("Game Over")
     if meteoralive:
         moveSprite(meteor, meteorX, meteorY, True)
         meteorX = meteorX + math.cos(Rad(meteorangle - 90))
         meteorY = meteorY + math.sin(Rad(meteorangle - 90))
-    if launch:
-        if math.tan(Rad(z)) > 0:
-            rocketX += 1
-        else:
-            rocketX -= 1
+    if not (0 < rocketrX < 980 and 0 < rocketrY < 638):
+        moveSprite(rocket, 480, 319, True)
+        killSprite(rocket)
+        launch = False
+    elif 0 < rocketrX < 980 and 0 < rocketrY < 638 and launch == True:
+        rotateSprite(rocket, -y)
+        rocketrX = rocketrX + math.cos(Rad(rocketangle - y + 180))
+        rocketrY = rocketrY + math.sin(Rad(rocketangle - y + 180))
+        moveSprite(rocket, rocketrX, rocketrY, True)
+        showSprite(rocket)
 
-        # if z == 90:
-        #     rocketX = 480
-        #     rocketY -= 1
-        # elif z == 270:
-        #     rocketX = 480
-        #     rocketY += 1
-        # else:
-        rocketY = math.tan(Rad(z)) * rocketX
-        foo = 480 + rocketX
-        bar = 319 - rocketY
-        print(foo, bar, math.tan(Rad(z)), z, x, y)
-        moveSprite(rocket, foo, bar, True)
-        pause(50)
-        # rocketrX = rocketrX + math.cos(Rad(meteorangle - 90))
-        # rocketrY = rocketrY + math.sin(Rad(meteorangle - 90))
-    fps = tick(60)
+        #    if math.tan(Rad(z)) > 0:
+    #        rocketX += 1
+    #
+    #    else:
+    #        rocketX -= 1
+    #    if z == 90:
+    #        z-=0.000000000000000000000000000001
+    #        rocketX = 480
+    #        rocketY -= 1
+    #    elif z == 270:
+    #        rocketX = 480
+    #        rocketY += 1
+    #    else:
+    #        rocketY = math.tan(Rad(z)) * rocketX
+    #        #foo = 960 - rocketX
+    #        #bar = 638 - rocketY
+    #        #print(foo, bar, math.tan(Rad(z)), z, x, y)
+    #        #moveSprite(rocket, foo, bar, True)
+    #        pause(50)
+    # rocketrX = rocketrX + math.cos(Rad(meteorangle - 90))
+    # rocketrY = rocketrY + math.sin(Rad(meteorangle - 90))
     updateDisplay()
+    fps = tick(29)
     changeLabel(FPS, "FPS: {0}".format(str(round(fps, 2))))
